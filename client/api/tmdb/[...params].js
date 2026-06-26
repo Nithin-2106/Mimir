a88c01cc0ef4b14f68cf1338f7c1f391aa8d00fa
Nithin <nithin21.v@gmail.com>
2026-06-26 23:36:11 +0530

    chatgpt tried something

--- client/api/tmdb/[...params].js
+++ client/api/tmdb/[...params].js
@@ -1,40 +1,8 @@
 import axios from 'axios'
 
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS')
-  if (req.method === 'OPTIONS') return res.status(200).end()
-
-  try {
-    const { params, ...query } = req.query
-
-    const tmdbPath = Array.isArray(params) ? params.join('/') : params
-
-    if (!tmdbPath) {
-      return res.status(400).json({ message: 'No path provided' })
-    }
-
-    const tmdbKey = process.env.TMDB_KEY
-
-    if (!tmdbKey) {
-      return res.status(500).json({ message: 'TMDB_KEY not configured' })
-    }
-
-    const url = `https://api.themoviedb.org/3/${tmdbPath}`
-
-    const response = await axios.get(url, {
-      params: {
-        ...query,
-        api_key: tmdbKey,
-      },
-      timeout: 10000,
-    })
-
-    return res.json(response.data)
-
-  } catch (err) {
-    return res.status(err.response?.status || 500).json({
-      message: err.response?.data?.status_message || err.message,
-    })
-  }
+  return res.status(200).json({
+    url: req.url,
+    query: req.query,
+  });
 }
